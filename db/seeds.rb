40a9e2da6de4e683d392690c84f5561dbbeb4bb1
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-30 12:02:27 +0100

    created the greetings model and added route to the controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greeting_1 = Message.create(greeting: "Hello Adorn, How are you")
+greeting_2 = Message.create(greeting: "Hello Mithi, Nice to meet you")
+greeting_3 = Message.create(greeting: "Hi Jihana, Whatsapp")
+greeting_4 = Message.create(greeting: "Hello Dejan, Lovely to see you today")
+greeting_5 = Message.create(greeting: "Hi Nuri, Hope you are having a lovely week!")
